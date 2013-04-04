587a4f2f47e157a0f115892e601b0a62a403c29c
Jane Sternbach <janesternbach@gmail.com>
2013-04-04 15:00:28 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,13 @@ Test.delete_all
 User.delete_all
 
 # User :address, :balance, :email, :first, :image, :is_admin, :last, :lat, :lon, :occupation, :password, :password_digest, :phone
+# Exams :name, :fee, :pass_rate, :creator_id
+
 
 User.create(email: 'admin@gmail.com', is_house: true, password: 'a', password_confirmation: 'a')
 u1 = User.create(first: 'Bob', last: 'Bobberson', password: 'a', password_confirmation: 'a')
+
+
+e1 = Exam.create(name:"Ruby Basics", fee:false, pass_rate:70, creator_id:u1.id)
+
+q1 = Question.create()
