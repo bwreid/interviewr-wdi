6dc6c4272a86426917627a45db81abe5785c84b3
Bryan W. Reid <bwreid@gmail.com>
2013-04-04 14:28:05 -0400

    pushing up user and runs testing

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: exams
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  fee        :boolean
+#  pass_rate  :integer
+#  creator_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Exam < ActiveRecord::Base
   attr_accessible :creator_id, :fee, :name, :pass_rate
+  has_many :runs
+  has_many :questions
 end
--- spec/models/exam_spec.rb
+++ spec/models/exam_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: exams
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  fee        :boolean
+#  pass_rate  :integer
+#  creator_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Exam do
