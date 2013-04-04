98861e640b7405b9e8f7caf573502cf3134c706a
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-04-04 14:49:53 -0400

    fixed tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404181421) do
+ActiveRecord::Schema.define(:version => 20130404183224) do
 
   create_table "choices", :force => true do |t|
     t.string   "text"
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,11 +4,7 @@ FactoryGirl.define do
     last "Bobberson"
     email "bob@gmail.com"
     address "10 E 21st St, New York, NY"
-    first "MyString"
-    last "MyString"
-    email "MyString"
     is_house false
-    address "MyString"
     phone "MyString"
     occupation "MyString"
     balance "9.99"
