baaabb48e6f9154384f2de220af4f8fa22ddb1d8
Bryan W. Reid <bwreid@gmail.com>
2013-04-05 09:35:58 -0400

    fixing tests

--- spec/factories/responses.rb
+++ spec/factories/responses.rb
@@ -1,6 +1,11 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  factory :response, class: Response do
+    answer false
+    run_id 1
+    question_id 1
+  end
   factory :response_a, class: Response do
     answer false
     run_id 1
