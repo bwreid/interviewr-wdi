270001083dc339036f2deb758d653011b6f1b3a0
Shefali Friesen <musictechgirl@gmail.com>
2013-04-05 11:36:48 -0400

    index page'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @exams = Exam.all
+    @questions = Question.all
+    @choices = Choice.all
+    @tags = Tag.all
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Interviewr::Application.routes.draw do
   root :to => 'home#index'
+  resources :exams do
+    get 'filter/'
+
+end
+
+resources :questions do
+  collection do
+    get 'filter/', :action => :filter, :as => :filter
+  end
+  collection do
+    get 'search'
+  end
 end
